5a813c03bfd42f62fba158621e05b0e948a525ba
biennguyen13 <40268416+biennguyen13@users.noreply.github.com>
2019-05-14 11:24:49 +0700

    1

---
+++ Assets/_Complete-Game/Scripts/Camera/CameraFollow.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace CompleteProject
+{
+    public class CameraFollow : MonoBehaviour
+    {
+        public Transform target;            // The position that that camera will be following.
+        public float smoothing = 5f;        // The speed with which the camera will be following.
+        public float topLimit = 12f;
+        public float bottomLimit = -8f;
+
+        //Vector3 offset;                     // The initial offset from the target.
+        float m;
+        float x;
+
+        void Start ()
+        {
+            // Calculate the initial offset.
+      //      offset = transform.position - target.position;
+        }
+
+        void FixedUpdate ()
+        {
+            //// Create a postion the camera is aiming for based on the offset from the target.
+            //Vector3 targetCamPos = target.position + offset;
+
+            //// Smoothly interpolate between the camera's current position and it's target position.
+            //transform.position = Vector3.Lerp (transform.position, targetCamPos, smoothing * Time.deltaTime);
+
+            m = -Input.GetAxis("Mouse Y");
+
+            x = transform.localEulerAngles.x;
+
+            if (x > topLimit) x = x - 360;
+
+            if (x + m > topLimit || x + m < bottomLimit) return;
+
+            transform.Rotate(m, 0, 0);
+
+        }
+    }
+}
---
+++ Assets/_Complete-Game/Scripts/Managers/EnemyManager.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+namespace CompleteProject
+{
+    public class EnemyManager : MonoBehaviour
+    {
+        public PlayerHealth playerHealth;       // Reference to the player's heatlh.
+        public GameObject[] enemy;                // The enemy prefab to be spawned.
+        public float spawnTime = 3f;            // How long between each spawn.
+        public Transform[] spawnPoints;         // An array of the spawn points this enemy can spawn from.
+
+       public int enemyAmoun = 0;
+        public int enemyMax = 10;
+
+        public int EnemyAmoun
+        {
+            get
+            {
+                return enemyAmoun;
+            }
+
+            set
+            {
+                enemyAmoun = value;
+            }
+        }
+
+        void Start ()
+        {
+            // Call the Spawn function after a delay of the spawnTime and then continue to call after the same amount of time.
+            InvokeRepeating ("Spawn", spawnTime, spawnTime);
+        }
+
+
+        void Spawn ()
+        {
+            // If the player has no health left...
+            if(playerHealth.currentHealth <= 0f || EnemyAmoun == enemyMax)
+            {
+                // ... exit the function.
+                return;
+            }
+
+            // Find a random index between zero and one less than the number of spawn points.
+            int spawnPointIndex = Random.Range (0, spawnPoints.Length);
+
+            int spawnEnemytIndex = Random.Range(0, enemy.Length);
+            // Create an instance of the enemy prefab at the randomly selected spawn point's position and rotation.
+            Instantiate (enemy[spawnEnemytIndex], spawnPoints[spawnPointIndex].position, spawnPoints[spawnPointIndex].rotation);
+
+            EnemyAmoun++;
+        }
+    }
+}
